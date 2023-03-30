75b61f26cb4d974afea3beb301fffe86f0efa41c
PRETTYGBEMI <gbemisola_k@yahoo.com>
2023-03-30 13:14:44 -0700

    feat: added new feature

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,6 +3,15 @@
 
 #include <stdlib.h>
 
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
 typedef struct list_s
 {
 	char *str;
@@ -15,5 +24,4 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-
 #endif
